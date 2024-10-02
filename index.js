79ba33ae9e70c77dbd1517dc4c780d666cee5e41
blessed wesonga <blessedwesonga@gmail.com>
2024-10-02 19:40:37 +0300

    phase-1-arrow-functions

---
+++ index.js
@@ -0,0 +1,16 @@
+// Function expression for divide
+const divide = function() {
+    return 2000 / 100;
+};
+console.log(divide()); //=> 20
+
+// Arrow function for square
+const square = x => x * x;
+console.log(square(4)); //=> 16
+// Arrow function for add
+const add = (a, b) => a + b;
+
+// Testing the function
+console.log(add(3, 5)); // Output: 8
+console.log(add(10, 20)); // Output: 30
+
